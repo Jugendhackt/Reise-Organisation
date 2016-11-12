bd2c8081e4c3eee0126dbc3734063e741985f9f7
paalwi <paulawiesner@gmx.net>
2016-11-12 22:00:17 +0900

    verbesserung

--- app.py
+++ app.py
@@ -4,16 +4,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-
 def hello_world():
     return 'Hallo Welt'
 
-@app.route('/zug/<stadt1>/<stadt2>')
+@app.route('/zug')
+def zug():
+    return bahn_api.anJson()
 
+@app.route('/zug/<stadt1>/<stadt2>')
 def zug(stadt1, stadt2):
     return bahn_api.anJson()
 
 @app.route('/wetter')
-
 def wetter():
     return wetter_api.wettervorhersage()
