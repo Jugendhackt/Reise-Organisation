39279e8198f1a4f7170b2d65226fcfe9e837db5a
paalwi <paulawiesner@gmx.net>
2016-11-12 17:27:31 +0900

    mit Json

--- bahn_api.py
+++ bahn_api.py
@@ -17,45 +17,67 @@ def bahnhofsInfo():
         bahnhoefe.append(bahnhof["name"])
     return bahnhoefe
 
-def von_nach(zuhause, urlaub):
-    #hinfahrt = input("an welchem hinfahrt moechtest du fahren? (DD.MM.YYYY)")
+def von_nach():
     hinfahrt = "20.11.2016"
-    hinfahrt = hinfahrt.split(".")
-    for d in hinfahrt:
-        d = int(d)
+    zuhause = "Karlsruhe"
+    urlaub = "Mannheim"
+    rueckfahrt = "22.11.2016"
+
+    hinverbindung = anfrage(zuhause, urlaub, hinfahrt)
+    rueckverbindung = anfrage(urlaub, zuhause, rueckfahrt)
+    gesamtpreis = int(hinverbindung["preis"]) + int(rueckverbindung["preis"])
+    del hinverbindung["preis"]
+    del rueckverbindung["preis"]
+    antwort = {}
+    antwort["zuhause"] = zuhause
+    antwort["urlaub"] = urlaub
+    antwort["hin"] = hinverbindung
+    antwort["rueck"] = rueckverbindung
+    antwort["preis"] = gesamtpreis
+
+    return antwort
+
+
+
+
+
+
+def anfrage(zuhause, urlaub, fahrt):
+    fahrt = fahrt.split(".")
     #uhrzeit = input("Um wieviel uhr moechtest du fahren?(HH:MM)")
-    uhrzeit = "10:00"
-    hinfahrt = hinfahrt[2] + "-" + hinfahrt[1] + "-" + hinfahrt[0]
+    uhrzeit = "02:00"
+    fahrt = fahrt[2] + "-" + fahrt[1] + "-" + fahrt[0]
 
 
     basis2 = "http://transport.opendata.ch/v1/connections"
     #zuhause = input("Abfahrtsbahnhof")
     #urlaub = input("Ankunftsbahnhof")
-    anfrage = basis2 + "?from=" + zuhause + "&to=" + urlaub + "&time=" + uhrzeit + "&date=" + hinfahrt
-    print(anfrage)
+    anfrage = basis2 + "?from=" + zuhause + "&to=" + urlaub + "&time=" + uhrzeit + "&date=" + fahrt
     r = requests.get(anfrage)
     antwort = r.json()
     alle_verbindungen = []
     for verbindung in antwort["connections"]:
         #pprint.pprint(verbindung)
         einzel= {}
-        einzel["abfahrtzeit"] = datetime.fromtimestamp(int(verbindung["from"]["departureTimestamp"]))
-        einzel["abfahrtsort"] = verbindung["from"]["station"]["name"]
-        einzel["ankunftszeit"] = datetime.fromtimestamp(int(verbindung["to"]["arrivalTimestamp"]))
-        einzel["ankunftsort"] = verbindung["to"]["station"]["name"]
+        abfahrt = datetime.fromtimestamp(int(verbindung["from"]["departureTimestamp"]))
+        einzel["abfahrtzeit"] = abfahrt.strftime("%Y-%m-%d %H:%M:%S")
+        ankunft = datetime.fromtimestamp(int(verbindung["to"]["arrivalTimestamp"]))
+        einzel["ankunftszeit"] = ankunft.strftime("%Y-%m-%d %H:%M:%S")
         einzel["umstiege"] = verbindung["transfers"]
         einzel["zugart"] = verbindung["products"]
-        zeit = einzel["ankunftszeit"] - einzel["abfahrtzeit"]
+        zeit = ankunft - abfahrt
         zeit = zeit.total_seconds()
         einzel["reisezeit"] = zeit / 60
         einzel["preis"] = einzel["reisezeit"] * 0.5
         alle_verbindungen.append(einzel)
 
-
+    zeit2 = 100000
     for verbindung in alle_verbindungen:
-        if verbindung["reisezeit"] < 100000:
-            zeit = verbindung["reisezeit"]
+        if verbindung["reisezeit"] < zeit2:
+            zeit2 = verbindung["reisezeit"]
             schnellste = verbindung
-    pprint.pprint(schnellste)
+    return schnellste
 
-von_nach()
+fahrplan = von_nach()
+with open('fahrplan.json', 'w') as fplan:
+    json.dump(fahrplan, fplan)
