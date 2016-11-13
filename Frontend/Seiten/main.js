042b2e906c50b7841715b709fc9dec0e0c0c460d
Ikarulus <github.ikarulus@zer.one>
2016-11-13 02:14:08 +0100

    working tech demo

--- Frontend/Seiten/main.js
+++ Frontend/Seiten/main.js
@@ -1,10 +1,15 @@
-(function($){
 function displayVals() {
-var singleValues = $( "#startbahnhof" ).val();
-var singleValues = $( "#startbahnhof" ).val();
-$( "#output" ).html( "<b>Result:</b> " + singleValues);
+  if ($( "#startbahnhof" ).val()=="" || $( "#zielbahnhof" ).val()=="") {
+    return false;
+  }
+  var eingabe = [$( "#unterkunft" ).val(), $( "#kultur" ).val(),
+  $( "#preisklasse" ).val(), $( "#kultur" ).val(), $( "#startbahnhof" ).val(),
+  $( "#zielbahnhof" ).val()];
+  console.log("test");
+  $.ajax({url: "http://128.199.88.103:5000/zug2/"+eingabe[4]+"/"+eingabe[5],
+  success: function(result){
+    $( "#output" ).html( "<b>Result:</b> " + result);
+  }});
 }
-
-$( "#startbahnhof" ).change( displayVals );
+$( "#formular" ).change( displayVals );
 displayVals();
-})(jQuery);
